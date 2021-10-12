822efd34729797b93e259d359eae479b015517eb
Spartan082 <cruch341@live.kutztown.edu>
2021-10-12 00:37:19 -0400

    yes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : "galliedb.c1j5afoliojj.us-east-1.rds.amazonaws.com",
+  user     : "admin",
+  password : "#Gallie143",
+  port     : "3306"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
+
 function App() {
   return (
     <div className="App">
